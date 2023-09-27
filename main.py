ce702e716255d057d40f82d5e933cd56a80c6563
estouan <120207575+estouan@users.noreply.github.com>
2023-09-27 22:32:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#-------------------------importation des modules-------------------------#
+# -------------------------importation des modules-------------------------#
 import time
 
 import pyautogui
@@ -15,28 +15,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 import sqlite3
 
-
-
-#-------------------------définition des variables-------------------------#
+# -------------------------définition des variables-------------------------#
 
 driver = webdriver.Firefox()
-#idt = "unlapinrameur"
-#motdp = ";leslapins"
+# idt = "unlapinrameur"
+# motdp = ";leslapins"
 
-#idt2="QuantumScribe"
-#motdp2=";AgoraBot0"
-
+# idt2="QuantumScribe"
+# motdp2=";AgoraBot0"
 
 
 idt = "QuantumScribe"
 motdp = ";AgoraBot0"
 
-idt2="unlapinrameur"
-motdp2=";AgoraBot0"
+idt2 = "unlapinrameur"
+motdp2 = ";AgoraBot0"
 conn = sqlite3.connect('QR.db')
 
-#-------------------------définition des fonctions-------------------------#
 
+# -------------------------définition des fonctions-------------------------#
 
 
 class Database:
@@ -89,24 +86,13 @@ def get_html():
 
 
 # Définition de la fonction obtenir_reponse avec un argument question
-def obtenir_reponse(question):
-    # Création d'un curseur pour exécuter une requête SQL
-    cursor = conn.cursor()
-    
-    # Exécution de la requête SQL pour récupérer la réponse à la question donnée
-    cursor.execute('SELECT ReponseText FROM Reponses INNER JOIN Questions ON Reponses.QuestionID = Questions.QuestionID WHERE Questions.QuestionText = ?', (question,))
-    
-    # Récupération du résultat de la requête
-    result = cursor.fetchone()
-    
-    # Retourne la réponse si elle existe, sinon retourne None
-    return result[0] if result else None
+
 def phase():
     element_question = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, '.question-content b')))
     soup = BeautifulSoup(element_question.get_attribute('outerHTML'), 'html.parser')
     texte_question = soup.get_text()
-    #reponse = obtenir_reponse(texte_question)
+    # reponse = obtenir_reponse(texte_question)
     reponse = None
     if reponse:
         print("Réponse :", reponse)
@@ -114,10 +100,7 @@ def phase():
         bouton_reponse = driver.find_element(By.XPATH, expression_xpath)
         bouton_reponse.click()
     else:
-        largeur_ecran, hauteur_ecran = pyautogui.size()
-        x_centre = largeur_ecran // 2
-        y_centre = hauteur_ecran // 2
-        pyautogui.click(x_centre, y_centre)
+
         boutons = driver.find_elements(By.XPATH,
                                        '//button[contains(@class, "mat-raised-button") and contains(@class, "comic-serif-font")]')
 
@@ -128,124 +111,126 @@ def phase():
 
             # Attendre que tous les boutons soient cliquables
 
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//button[contains(@class, "mat-raised-button") and contains(@class, "comic-serif-font")]')))
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+                (By.XPATH, '//button[contains(@class, "mat-raised-button") and contains(@class, "comic-serif-font")]')))
 
             # Cliquez sur le bouton choisi
             bouton_choisi.click()
-            time.sleep(2)
-            largeur_ecran, hauteur_ecran = pyautogui.size()
-            x_centre = largeur_ecran // 2
-            y_centre = hauteur_ecran // 2
-            pyautogui.click(x_centre, y_centre)
+
         else:
             print("Aucun bouton trouvé")
 
 
 def partie():
-    for i in range(3):
-
-        phase()
-        time.sleep(4)
-    #apprantisage()
     try:
-        bouton_fermer = WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, '//button[contains(.,"Fermer")]')))
-        if bouton_fermer:
-            bouton_fermer.click()
+        print("on est la")
+        bouton_versus = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'main-container')))
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, 'main-container')))
+        bouton_versus.click()
+        driver.execute_script('document.elementFromPoint(window.innerWidth / 2, window.innerHeight / 2).click();')
+        print("ca passe")
     finally:
 
-        bouton_suivant = WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, '//button[contains(.,"Suivant")]')))
-        bouton_suivant.click()
-        time.sleep(2)
-        bouton_suivant = WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, '//button[contains(.,"Retour aux Parties en cours")]')))
-        if bouton_suivant:
+        boutons_similaires = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '.mat-raised-button.theme-button')))
+        boutons_similaires.click()
+        for i in range(3):
+            phase()
+            time.sleep(4)
+        # apprantisage()
+        try:
+            bouton_fermer = WebDriverWait(driver, 3).until(
+                EC.visibility_of_element_located((By.XPATH, '//button[contains(.,"Fermer")]')))
+            if bouton_fermer:
+                bouton_fermer.click()
+        finally:
+            bouton_suivant = WebDriverWait(driver, 3).until(
+                EC.visibility_of_element_located((By.XPATH, '//button[contains(.,"Suivant")]')))
             bouton_suivant.click()
-            print("partie fini a l'autre")
-        else:
-            bouton_suivant = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//button[contains(.," Manche Suivante ")]')))
-            if bouton_suivant:
-                bouton_suivant.click()
-                partie()
+            try:
+                bouton_retour = WebDriverWait(driver, 3).until(
+                    EC.visibility_of_element_located((By.XPATH, '//button[contains(.,"Retour aux Parties en cours")]')))
+                if bouton_retour:
+                    bouton_retour.click()
+                print("partie fini a l'autre")
+            except:
+                try:
+                    bouton_manche = WebDriverWait(driver, 10).until(
+                        EC.visibility_of_element_located((By.XPATH, '//button[contains(.," Manche Suivante ")]')))
+                    if bouton_manche:
+                        bouton_manche.click()
+                        partie()
+                except:
+
+                    print("erreur")
+            finally:
+                print("fin partie")
+
+
 # Définition de la fonction connection avec deux arguments idt et motdp
 
-def connection(idt,motdp):
-    
+def connection(idt, motdp):
     # Recherche de l'élément identifiant et envoi de la valeur de idt
     identifiant = driver.find_element(By.ID, "mat-input-0")
     identifiant.send_keys(idt)
-    
+
     # Recherche de l'élément mdp et envoi de la valeur de motdp
     mdp = driver.find_element(By.ID, "mat-input-1")
     mdp.send_keys(motdp)
-    
+
     # Recherche du bouton_connexion et clic dessus
-    bouton_connexion = driver.find_element(By.CLASS_NAME,"primary-button")
+    bouton_connexion = driver.find_element(By.CLASS_NAME, "primary-button")
     bouton_connexion.click()
     # Récupère l'URL actuelle de la page
     url_actuelle = driver.current_url
     WebDriverWait(driver, 10).until(EC.title_is("AgoraQuiz - Accueil du groupe"))
+    print(url_actuelle)
     # Si l'URL actuelle est "https://agora-quiz.education/Login", appelle la fonction connection avec les arguments idt et motdp pour se connecter au site
     if url_actuelle == "https://agora-quiz.education/HomeGroupe":
 
-    # Recherche du bouton_jouer et clic dessus après qu'il soit visible
-        bouton_jouer = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//button[contains(.,"Jouer des Parties")]')))
+        # Recherche du bouton_jouer et clic dessus après qu'il soit visible
+        bouton_jouer = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//button[contains(.,"Jouer des Parties")]')))
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//button[contains(.,"Jouer des Parties")]')))
         bouton_jouer.click()
         return True
     else:
         print("erreur connection")
         return False
 
-    
+
 # Définition de la fonction start_partie avec un argument idt2
 def start_partie(idt2):
-
     # Recherche de l'élément input_adversaire et envoi de la valeur de idt2
     input_adversaire = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "mat-input-3")))
     input_adversaire.send_keys(idt2)
-    
+
     # Recherche du bouton_inviter et clic dessus
-    bouton_inviter = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, '//button[contains(.,"Inviter")]')))
+    bouton_inviter = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//button[contains(.,"Inviter")]')))
     bouton_inviter.click()
     url_actuelle = driver.current_url
-    if url_actuelle != "https://agora-quiz.education/Games/List":
-        time.sleep(5)
-        largeur_ecran, hauteur_ecran = pyautogui.size()
-        x_centre = largeur_ecran // 2
-        y_centre = hauteur_ecran // 2
-        pyautogui.click(x_centre, y_centre)
-
-        # Recherche du bouton boutons_similaires et clic dessus
-        boutons_similaires = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, '.mat-raised-button.theme-button')))
-        boutons_similaires.click()
+    if url_actuelle != "https://agora-quiz.education/Games/List" and url_actuelle != "https://agora-quiz.education/HomeGroupe":
         partie()
     else:
         bouton_duel = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//button[contains(.,"'+idt2+'")]')))
+            EC.presence_of_element_located((By.XPATH, '//button[contains(.,"' + idt2 + '")]')))
         bouton_duel.click()
 
         url_actuelle = driver.current_url
         if url_actuelle != "https://agora-quiz.education/Games/List":
-            #passage debut duel ou on voit les tete
+            # passage debut duel ou on voit les tete
 
-            time.sleep(5)
-            largeur_ecran, hauteur_ecran = pyautogui.size()
-            x_centre = largeur_ecran // 2
-            y_centre = hauteur_ecran // 2
-            pyautogui.click(x_centre, y_centre)
-
-
-            boutons_similaires = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, '.mat-raised-button.theme-button')))
-            boutons_similaires.click()
             partie()
         else:
             print("erreur lancement de partie")
-    
+
     # Récupération de la taille de l'écran et clic au centre de l'écran
 
-    
     # Appel de la fonction partie()
-
 
     # Si l'URL actuelle est "https://agora-quiz.education/Login", appelle la fonction connection avec les arguments idt et motdp pour se connecter au site
 
@@ -258,13 +243,13 @@ url_actuelle = driver.current_url
 
 # Si l'URL actuelle est "https://agora-quiz.education/Login", appelle la fonction connection avec les arguments idt et motdp pour se connecter au site
 if url_actuelle == "https://agora-quiz.education/Login":
-    bool=connection(idt,motdp)
-    
+    bool = connection(idt, motdp)
+
 # Affiche "lapin" dans la console
 print("lapin")
 
 # Appelle la fonction start_partie avec l'argument idt2 pour commencer une partie
-if bool==True:
+if bool == True:
     start_partie(idt2)
 print("fin")
 time.sleep(3)
